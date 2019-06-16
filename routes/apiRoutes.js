6b2817805af63298571ef7f05867b71fdcf8c1d8
Paul Raab <pvraab@hotmail.com>
2019-06-16 15:16:55 -0600

    Add logic to find best match

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,9 +10,37 @@ module.exports = function (app) {
 
   // Post friends
   app.post("/api/friends", function (req, res) {
+    console.log("Post");
+    var match = checkForMatch(req.body);
+    console.log("Post");
+    console.log(match);
     friendData.push(req.body);
-    res.json(true);
+    res.json(match);
   });
+
+  function checkForMatch(friendObj) {
+    var compVals = 0;
+    var match = {
+      name: "",
+      photo: "",
+      score: ""
+    };
+    minVals = 40;
+    friendData.forEach(function (elem) {
+      elem.scores.forEach(function (score, index) {
+        compVals = parseInt(compVals) + Math.abs(parseInt(score) - parseInt(friendObj.scores[index]));
+      });
+      if (compVals < minVals) {
+        console.log("Min");
+        console.log(elem);
+        match.name = elem.name;
+        match.photo = elem.photo;
+        match.score = compVals;
+        minVals = compVals;
+      }
+    });
+    return match;
+  };
 
   // Clear friend data
   app.post("/api/clear", function (req, res) {
